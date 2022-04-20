707e9769fdf917907fa80b9a720443ad2c78bd87
shiran-trrzs <shirannnn.4@gmail.com>
2022-04-20 17:27:01 -0500

    Update:  dynamic select with api data

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
 const apiURL = 'https://bsaleonlinestore.herokuapp.com/';
+
+const HTMLResponse = document.querySelector('#filterSection');
+const selectOption = document.createElement('select');
+selectOption.setAttribute('id', 'chooseCategory');
+selectOption.setAttribute('class', 'chooseCategory');
+
+fetch(`${apiURL}category/`,)
+.then((res, rej) => res.json())
+.then((category) => {
+    category.forEach( cat => {
+        let categoryOption = document.createElement('option');
+        categoryOption.setAttribute('value', `${cat.name}`);
+        categoryOption.appendChild(
+            document.createTextNode(`${cat.name}`)
+        );
+        selectOption.appendChild(categoryOption)
+    });
+
+    HTMLResponse.appendChild(selectOption);
+})
